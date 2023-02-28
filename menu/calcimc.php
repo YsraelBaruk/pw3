ae3ec6b3ae865bbfa906427c4d2606679a909677
IsraelBaruque <israekbaruque992@gmail.com>
2023-02-28 11:15:07 -0300

    sobe

--- menu/calcimc.php
+++ menu/calcimc.php
@@ -12,19 +12,19 @@
         $altura/=100;
         $resultado = ($kilos / ($altura * $altura));
 
-        if($resultado < 18.5){
+        if($resultado <= 18.5){
             echo "<h3>Abaixo do Peso <br> IMC: $resultado</h3>";
         }
-        else if($resultado < 24.9){
+        else if($resultado <= 24.9){
             echo "<h3>Peso normal <br> IMC: $resultado</h3>";
         }
-        else if($resultado < 29.9){
+        else if($resultado <= 29.9){
             echo "<h3>Excesso de peso <br> IMC: $resultado</h3>";
         }
-        else if($resultado < 34.9){
+        else if($resultado <= 34.9){
             echo "<h3>Obesidade Classe I <br> IMC: $resultado</h3>";
         }
-        else if($resultado < 39.9){
+        else if($resultado <= 39.9){
             echo "<h3>Obesidade Classe II <br> IMC: $resultado</h3>";
         }
         else{
